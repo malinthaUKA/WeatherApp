63e9bbc8f9447824379e3e725a491a2847feda75
Malintha Randika <hettigemalintha@gmail.com>
2024-06-04 23:44:55 +0530

    make UI design such as input fields and button

--- app/src/main/java/com/example/weather_app/MainActivity.java
+++ app/src/main/java/com/example/weather_app/MainActivity.java
@@ -3,8 +3,15 @@ package com.example.weather_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
